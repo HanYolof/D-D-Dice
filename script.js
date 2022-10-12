f1a91fcc9efa28f0e9c04bd93a490d0c3df3ef3c
HanYolof <akseli.asik@gmail.com>
2022-10-12 16:04:06 +0300

    renamed branch

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ const convert = () => {
 	return totalArray
 }
 
-//
+//Extras
+
+
 
 
 
--- script2.js
+++ script2.js
@@ -1,6 +1,7 @@
 //D&D Dice front end elements
 
 //Buttons
+
 const dice4 = document.getElementById('4sided').addEventListener('click', () => {total = diceRoll(4, Number(document.getElementById('extra').value)); convert(); outcomeToFront();outcomeToFrontArray();});
 const dice6 = document.getElementById('6sided').addEventListener('click', () => {total = diceRoll(6, Number(document.getElementById('extra').value)); convert(); outcomeToFront();outcomeToFrontArray();});
 const dice8 = document.getElementById('8sided').addEventListener('click', () => {total = diceRoll(8, Number(document.getElementById('extra').value)); convert(); outcomeToFront();outcomeToFrontArray();});
@@ -12,9 +13,9 @@ const diceCustom = document.getElementById('custom').addEventListener('click', (
 //Calculations to front end
 
 const outcomeToFront = () => {
-	totalToFront = document.getElementById('total').appendChild(document.createElement('p'));
-	totalToFront.textContent = total;}
+	totalToFront = document.getElementById('total').appendChild(document.createTextNode('p'));
+	totalToFront.textContent = total + "-";}
 
 const outcomeToFrontArray = () => {
-	totalToFrontArray = document.getElementById('rolls').appendChild(document.createElement('p'));
-	totalToFrontArray.textContent = totalArray;}
+	totalToFrontArray = document.getElementById('rolls').appendChild(document.createTextNode('p'));
+	totalToFrontArray.textContent = totalArray + "-";}
