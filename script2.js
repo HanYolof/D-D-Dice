799f666ee9cff07eb8c7803b0bb7522808553ba9
HanYolof <akseli.asik@gmail.com>
2022-10-12 15:32:24 +0300

    updated

--- script2.js
+++ script2.js
@@ -2,12 +2,12 @@
 
 //Buttons
 const dice4 = document.getElementById('4sided').addEventListener('click', () => {total = diceRoll(4, Number(document.getElementById('extra').value)); convert(); outcomeToFront();outcomeToFrontArray();});
-const dice6 = document.getElementById('6sided').addEventListener('click', () => {total = diceRoll(6, Number(document.getElementById('extra').value)); convert();});
-const dice8 = document.getElementById('8sided').addEventListener('click', () => {total = diceRoll(8, Number(document.getElementById('extra').value)); convert();});
-const dice12 = document.getElementById('12sided').addEventListener('click', () => {total = diceRoll(12, Number(document.getElementById('extra').value)); convert();});
-const dice20 = document.getElementById('20sided').addEventListener('click', () => {total = diceRoll(20, Number(document.getElementById('extra').value)); convert();});
+const dice6 = document.getElementById('6sided').addEventListener('click', () => {total = diceRoll(6, Number(document.getElementById('extra').value)); convert(); outcomeToFront();outcomeToFrontArray();});
+const dice8 = document.getElementById('8sided').addEventListener('click', () => {total = diceRoll(8, Number(document.getElementById('extra').value)); convert(); outcomeToFront();outcomeToFrontArray();});
+const dice12 = document.getElementById('12sided').addEventListener('click', () => {total = diceRoll(12, Number(document.getElementById('extra').value)); convert(); outcomeToFront();outcomeToFrontArray();});
+const dice20 = document.getElementById('20sided').addEventListener('click', () => {total = diceRoll(20, Number(document.getElementById('extra').value)); convert(); outcomeToFront();outcomeToFrontArray();});
 
-const diceCustom = document.getElementById('custom').addEventListener('click', () => {totalArray = Array.from({length:document.getElementById('dices').value},() => diceRoll(document.getElementById('sides').value, Number(document.getElementById('extra').value)));total = totalArray.reduce((a, b)=> a + b, 0);outcomeToFrontArray();});
+const diceCustom = document.getElementById('custom').addEventListener('click', () => {totalArray = Array.from({length:document.getElementById('dices').value},() => diceRoll(document.getElementById('sides').value, Number(document.getElementById('extra').value)));total = totalArray.reduce((a, b)=> a + b, 0);outcomeToFront();outcomeToFrontArray();});
 
 //Calculations to front end
 
